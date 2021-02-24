c273d64e8532866b9a288a37ba31563057d39b1d
99003785 <sourav.dey@ltts.com>
2021-02-24 14:32:56 +0530

    test file

--- 99003785/test1.py
+++ 99003785/test1.py
@@ -1,24 +1,26 @@
 import re
 count = 0
 str1 = " "
-file_inp_1 = open("input.txt")
-word_searched = input("Enter the word:\n")
-file_line = file_inp_1.read()
-file_list = file_line.split()
-file_name = word_searched+"txt"
+input_file = open("D:\99003785\DOC\input.txt")
+input_word_searched = input("Enter the word:\n")
+file_name = input_word_searched+"txt"
 file_out = open(file_name, 'w')
+file_line = input_file.read()
+file_list = file_line.split()
+file_1 = re.split(r"\W",str(file_list))
 
-for i in range(len(file_list)):
-    match = re.match(word_searched, file_list[i], re.M|re.I)
+
+for i in range(len(file_1)):
+    match = re.fullmatch(input_word_searched, file_1[i], re.M|re.I)
     if match:
         count += 1
-        str1 = file_list[i-1]+" "+file_list[i]+" "+file_list[i+1]
+        str1 = file_1[i-4]+" "+file_1[i]+" "+file_1[i+4]
         file_out.write(str1)
         file_out.write("\n")
 
 print(count)
 file_out.write("count="+str(count))
-file_inp_1.close()
+input_file.close()
 file_out.close()
 
 
