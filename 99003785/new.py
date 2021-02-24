fbb4cbc63bf0a201d6c71359566af179fb6a9f34
99003785 <sourav.dey@ltts.com>
2021-02-24 18:28:25 +0530

    test

--- 99003785/new.py
+++ 99003785/new.py
@@ -1,12 +1,7 @@
-import re
-input_1=input("Enter the word:\n")
-file=open("D:\99003785\DOC\input.txt",'r')
+class MyProject:
+    def __init__(self):
+        self.input_word_1 = input("Enter the first word to be searched:\n")
+        self.input_word_2 = input("Enter the second word to be searched:\n")
 
-count=1
-read_line=file.readlines()
-for line in read_line():
-    list_1=re.findall(input_1,line,re.M|re.I)
-    if list_1:
-        count+=1
 
-print(count)
+
--- 99003785/test1.py
+++ 99003785/test1.py
@@ -5,14 +5,15 @@ file_inp_1 = open("input.txt")
 word_searched = input("Enter the word:\n")
 file_line = file_inp_1.read()
 file_list = file_line.split()
+file_list_2 = re.split(r"\w", str(file_list))
 file_name = word_searched+"txt"
 file_out = open(file_name, 'w')
 
-for i in range(len(file_list)):
-    match = re.match(word_searched, file_list[i], re.M|re.I)
+for i in range(len(file_list_2)):
+    match = re.fullmatch(word_searched, file_list_2[i], re.M | re.I)
     if match:
         count += 1
-        str1 = file_list[i-1]+" "+file_list[i]+" "+file_list[i+1]
+        str1 = file_list_2[i-4]+" "+file_list_2[i]+" "+file_list_2[i+4]
         file_out.write(str1)
         file_out.write("\n")
 
