e0384a4ddba0a931d44ddd61cacb43e10a667397
99003785 <sourav.dey@ltts.com>
2021-02-24 12:38:38 +0530

    changes

--- Day1/test.py
+++ Day1/test.py
@@ -1,8 +1,8 @@
 import re
 count= 0
-software=[]
+software=list()
 lines=0
-
+list_of_lines=[]
 pattern=re.compile("Software",re.IGNORECASE)
 
 with open("D:\99003785\DOC\input.txt",'rt') as file_info:
@@ -12,16 +12,16 @@ with open("D:\99003785\DOC\input.txt",'rt') as file_info:
         
         if pattern.search(file_line)!= None:
             software.append((lines,file_line.rstrip('\n')))
+           ''' for line in software:
+                stripped_line = line.strip()
+                line_list = stripped_line.split()
+                list_of_lines.append(line_list)
+        print(list_of_lines)'''
 
-                
+                            
     for answer in software:
         count+=1
-        print(answer[1]+'\n')
-        for word in answer:
-            print(word)
         
-
-
         with open("software.txt",'a') as file_answer:
             file_answer.writelines(str(count)+':')
             file_answer.writelines(answer[1]+'\n')        
